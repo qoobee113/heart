696e035883843cadf37d64f00f358cd5b47a9574
phuc.pham <phuc.pham@idtek.com.vn>
2021-06-22 20:22:18 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/',(req,res,next)=>{
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+app.listen(process.env.PORT || 3000,function () {
+    console.log('Sever Is Running');
+});
