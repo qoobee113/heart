f67b6acfc0c436720a662a1fa243e325d98ed75b
phuc.pham <phuc.pham@idtek.com.vn>
2021-06-22 21:04:24 +0700

    a

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use('/', express.static(path.join(__dirname, 'public')))
 app.get('/',(req,res,next)=>{
     res.sendFile(path.join(__dirname, '/index.html'));
 })
